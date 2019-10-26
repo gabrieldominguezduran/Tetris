79329296988f05f3c268d46899283ee0d42348c4
Gabriel Domínguez Durán <gabrieldominguezduran@gmail.com>
2019-10-26 19:36:10 +0200

    Updates

---
+++ precache-manifest.387d08487a7b40ca447a2ed8e7352943.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "decdbd3e712ee08c619e6d39f4f10d87",
+    "url": "/Tetris/index.html"
+  },
+  {
+    "revision": "feb8dd3e15bcf47abe0c",
+    "url": "/Tetris/static/css/main.9cf843ff.chunk.css"
+  },
+  {
+    "revision": "d86e34e4d683e9dfcfe1",
+    "url": "/Tetris/static/js/2.e5a50e50.chunk.js"
+  },
+  {
+    "revision": "feb8dd3e15bcf47abe0c",
+    "url": "/Tetris/static/js/main.a83d0312.chunk.js"
+  },
+  {
+    "revision": "1a06ce9582c07a906640",
+    "url": "/Tetris/static/js/runtime-main.ab2759c3.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/Tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "a3d76f079c4386692c988828506879ea",
+    "url": "/Tetris/static/media/tetris.a3d76f07.jpg"
+  }
+]);
